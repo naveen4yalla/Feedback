55dddbfb2f29ec75cfebe1adb33b7036d432d166
naveen4yalla <yvnaveen3120@gmail.com>
2023-05-11 00:43:37 -0700

    Initial Commit

---
+++ SpringApp/SpringAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpringAppApp.swift
+//  SpringApp
+//
+//  Created by Naveen Yalla on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SpringAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
